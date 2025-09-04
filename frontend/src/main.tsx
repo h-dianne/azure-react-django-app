8b5cf59f58a39e15ab62bdadb2d8f2f07b9496c1
h-dianne <diannehidalgo117@gmail.com>
2025-09-04 17:10:07 +0900

    feat: Integrate MSAL for Azure AD authentication in main entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./config/authConfig.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import "./styles/index.css";
+import { App } from "./App.tsx";
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+createRoot(document.getElementById("root")!).render(
+  <MsalProvider instance={msalInstance}>
     <App />
-  </StrictMode>,
-)
+  </MsalProvider>
+);
