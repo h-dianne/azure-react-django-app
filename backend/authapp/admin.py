ce407d970dc9fd297918ff91c716de9507b720fe
h-dianne <diannehidalgo117@gmail.com>
2025-09-08 09:57:53 +0900

    feat: Create Django project structure

--- backend/authapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserToken
-
-class UserTokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'token', 'created_at', 'blacklisted')
-    list_filter = ('blacklisted',) 
-    search_fields = ('user__username', 'token')  
-
-admin.site.register(UserToken, UserTokenAdmin)
--- backend/authapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import DashboardView, UserLogout
-
-urlpatterns = [
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('logout/', UserLogout.as_view(), name='logout'),
-]
